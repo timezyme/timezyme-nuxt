34c37c00082188c7546e7a5ce8714500b8aa1fe4
Stephen Pasco <github@timezyme.com>
2025-05-23 09:37:13 -0500

    :sparkles: setup tailwind / daisy ui / icons

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1 class="text-3xl text-center font-bold underline">
+    Hello world!
+  </h1>
+  <button class="btn btn-primary">
+    View Map <Icon name="tabler:map" />
+  </button>
 </template>

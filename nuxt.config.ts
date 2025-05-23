42bec40e543b4c9e4923c428caac88a934110759
TimeZyme <github@timezyme.com>
2025-05-23 09:58:35 -0500

    :sparkles: setup tailwind / daisy ui / icons (#11)
    Closes #11

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
   },
 });
